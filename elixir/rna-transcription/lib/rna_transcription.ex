be384630352740eb8c02dc72ba8a085a9ef8e002
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-05 19:24:25 -0400

    elixir

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -7,7 +7,12 @@ defmodule RnaTranscription do
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
   """
+
+  @pairs %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
+
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    Enum.map(dna, &(@pairs[&1]))
   end
+
 end
