8d77a217fb6ad18d5c13e075a2573719fc60e97e
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-13 09:40:20 -0400

    go hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,22 @@
 package hamming
 
+import "errors"
+
+// Distance func compares each letter in strings a and b
+// and returns the number of differences
 func Distance(a, b string) (int, error) {
+
+	if len(a) != len(b) {
+		return 0, errors.New("invalid")
+	}
+
+	count := 0
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+
+	return count, nil
 }
