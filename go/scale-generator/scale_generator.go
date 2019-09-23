1b2625e6ce231cee42e673d1bad02c49987f0c9f
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-09-23 19:57:56 -0400

    list comprehensions in eex

---
+++ go/scale-generator/scale_generator.go
@@ -0,0 +1,11 @@
+package scale
+
+
+var chromaticSharp = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
+
+var chromaticFlat = []string{"F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"}
+
+// Scale generator.
+func Scale(tonic, interval string) []string {
+	//
+}
