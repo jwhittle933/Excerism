fab4de848188bd5ce1ca42c73cea218c1bb14c63
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-11-18 09:28:54 -0500

    When Chuck Norris does a pushup, he isn't lifting himself up, he's pushing the Earth down.

--- go/scale-generator/scale_generator.go
+++ go/scale-generator/scale_generator.go
@@ -1,11 +1,19 @@
 package scale
 
-
 var chromaticSharp = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
 
 var chromaticFlat = []string{"F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"}
 
 // Scale generator.
 func Scale(tonic, interval string) []string {
-	//
+
+	if tonic == "C" && interval == "" {
+		return chromaticSharp
+	}
+
+	if tonic == "F" && interval == "" {
+		return chromaticFlat
+	}
+
+	return []string{"test"}
 }
---
+++ scheme/hello-world/test.scm
@@ -0,0 +1,94 @@
+(import (rnrs))
+
+(define test-fields '(input output who))
+
+(define (test-run-solution solution input)
+  (if (procedure? solution) (apply solution input) solution))
+
+(define (test-success description success-predicate
+         procedure input output)
+  (call/cc
+    (lambda (k)
+      (with-exception-handler
+        (lambda (e)
+          (k `(fail
+                (description . ,description)
+                (input . ,input)
+                (output . ,output)
+                (who . ,procedure))))
+        (lambda ()
+          (let ([result (test-run-solution procedure input)])
+            (unless (success-predicate result output)
+              (error 'exercism-test
+                "test fails"
+                description
+                input
+                result
+                output)))
+          `(pass . ,description))))))
+
+(define (test-error description procedure input)
+  (call/cc
+    (lambda (k)
+      (with-exception-handler
+        (lambda (e) (k `(pass . ,description)))
+        (lambda ()
+          (test-run-solution procedure input)
+          `(fail
+             (description . ,description)
+             (input . ,input)
+             (output . error)
+             (who . ,procedure)))))))
+
+(define (run-test-suite tests . query)
+  (for-each
+    (lambda (field)
+      (unless (and (symbol? field) (memq field test-fields))
+        (error 'run-test-suite
+          (format #t "~a not in ~a" field test-fields))))
+    query)
+  (let-values ([(passes failures)
+                (partition
+                  (lambda (result) (eq? 'pass (car result)))
+                  (map (lambda (test) (test)) tests))])
+    (cond
+      [(null? failures) (format #t "~%Well done!~%~%")]
+      [else
+       (format
+         #t
+         "~%Passed ~a/~a tests.~%~%The following test cases failed:~%~%"
+         (length passes)
+         (length tests))
+       (for-each
+         (lambda (failure)
+           (format
+             #t
+             "* ~a~%"
+             (cond
+               [(assoc 'description (cdr failure)) => cdr]
+               [else (cdr failure)]))
+           (for-each
+             (lambda (field)
+               (let ([info (assoc field (cdr failure))])
+                 (format #t "  - ~a: ~a~%" (car info) (cdr info))))
+             query))
+         failures)
+       (error 'test "incorrect solution")])))
+
+(define hello-world)
+
+(define (test . query)
+  (apply
+    run-test-suite
+    (list
+      (lambda ()
+        (test-success "Say Hi!" equal? hello-world '()
+          "Hello, World!")))
+    query))
+
+(let ([args (command-line)])
+  (if (null? (cdr args))
+      (load "hello-world.scm")
+      (load (cadr args)))
+  (test 'input 'output))
+
